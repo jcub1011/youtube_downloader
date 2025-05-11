cfc538a4d7faf77207924b931ed5c0d4449f31d9
jcub1011 <91000914+jcub1011@users.noreply.github.com>
2025-05-11 06:06:19 -0400

    Working on queue based download system.

--- lib/src/entities/video_retriever.dart
+++ lib/src/entities/video_retriever.dart
@@ -1,4 +1,8 @@
 import 'dart:collection';
+import 'dart:io';
+
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
 
 class DownloadRequestArgs {
   final String url;
@@ -21,15 +25,47 @@ class DownloadRequestArgs {
 class VideoDownloader {
   int _maxConcurrentDownloads; 
   /// Queue of videos to download.
-  Queue<DownloadRequestArgs> queue; 
+  final Queue<DownloadRequestArgs> _queue; 
+  final Queue<Client> _clientPool = Queue<Client>();
+  final Queue<Client> _activeClients = Queue<Client>();
 
-  VideoDownloader(this._maxConcurrentDownloads) : queue = Queue<DownloadRequestArgs>();
+  VideoDownloader(this._maxConcurrentDownloads) : 
+  _queue = Queue<DownloadRequestArgs>() 
+  {
+    while (_clientPool.length + _activeClients.length < _maxConcurrentDownloads) {
+      _clientPool.add(Client());
+    }
+  }
 
   set maxConcurrentDownloads(int max) {
     _maxConcurrentDownloads = max;
+    
+    while (_clientPool.length + _activeClients.length < _maxConcurrentDownloads) {
+      _clientPool.add(Client());
+    }
+
+    while (_clientPool.length + _activeClients.length > _maxConcurrentDownloads) {
+      if (_clientPool.isNotEmpty) {
+        _clientPool.removeLast().close();
+      } else {
+        break;
+      }
+    }
   }
 
   void addToQueue(DownloadRequestArgs args) {
-    queue.add(args);
+    _queue.add(args);
+
+    // Start download process if not already running.
+  }
+
+  void dispose() {
+    while (_clientPool.isNotEmpty) {
+      _clientPool.removeLast().close();
+    }
+
+    while (_activeClients.isNotEmpty) {
+      _activeClients.removeLast().close();
+    }
   }
 }
